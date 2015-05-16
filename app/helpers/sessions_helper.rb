3b30bd3524afb015d5e06e1b8f1f5b9687cbe1fd
Marco Blagoiev <marcoblagoiev@yahoo.com>
2015-05-16 17:03:05 +0300

    Forgetting users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,14 +25,22 @@ module SessionsHelper
     end
   end
 
- 
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
